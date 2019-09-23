17294bfb13d3c1fc4335239c34a0673030242785
Charles-Henri GUERIN <charleshenri.guerin@me.com>
2019-09-23 21:50:25 +0200

    :sparkles: Create blog (#3)
    * :construction: Create blog starter
    
    * :fire: Remove yarn.lock from blog
    
    * :memo: Ajoute premier article hello-world
    
    * :recycle: Ajoute plus d'information sur Pair-Roulette
    
    * :recycle: Créé une note de bas de page
    
    * :recycle: Reformulation "histoire en live"
    
    * supprime un "donc"
    * remplace un "donc" par "alors"
    
    * Apply suggestions from code review
    
    Co-Authored-By: Gautier de Saint Martin Lacaze <dark.jabberwock+github@gmail.com>
    
    * Apply suggestions from code review
    
    Co-Authored-By: claire-selosse <54957540+claire-selosse@users.noreply.github.com>
    Co-Authored-By: Yuket <39307501+Yuket@users.noreply.github.com>
    
    * Update packages/blog/gatsby-config.js
    
    Co-Authored-By: Yuket <39307501+Yuket@users.noreply.github.com>
    
    * Update packages/blog/content/posts/hello-world.mdx
    
    Co-Authored-By: claire-selosse <54957540+claire-selosse@users.noreply.github.com>
    
    * Update packages/blog/content/posts/hello-world.mdx
    
    Co-Authored-By: claire-selosse <54957540+claire-selosse@users.noreply.github.com>
    
    * Update hello-world.mdx
    
    * Update hello-world.mdx

---
+++ packages/blog/gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  siteMetadata: {
+    title: `Pair-Roulette`,
+    author: `Charles-Henri GUERIN`,
+    description: `Toutes les nouveautés sur Pair-Roulette.`,
+    social: [
+      {
+        name: `Charles-Henri GUERIN`,
+        url: `https://charlyx.dev`,
+      },
+      {
+        name: `Yvonnick FRIN`,
+        url: `https://yvonnickfrin.dev`,
+      },
+    ],
+  },
+}
